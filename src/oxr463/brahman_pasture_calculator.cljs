98a2f3e32ecb25b932a957156dc96d229ce4d39c
Lucas Ramage <ramage.lucas@protonmail.com>
2021-05-13 21:16:34 -0400

    Update brahman_pasture_calculator.cljs

--- src/oxr463/brahman_pasture_calculator.cljs
+++ src/oxr463/brahman_pasture_calculator.cljs
@@ -1,4 +1,15 @@
-(ns ^:figwheel-hooks oxr463.brahman-pasture-calculator)
+(ns ^:figwheel-hooks oxr463.brahman-pasture-calculator
+  (:require
+    [reagent.dom :as r.dom]))
 
-(defn ^:after-load re-render[]
- (js/console.log "Moo!"))
+(defn app []
+  [:h1.site__title
+    [:span.site__title-text "Brahman Pasture Calculator"]]])
+
+(defn mount []
+  (r.dom/render [app] (js/document.getElementById "root")))
+
+(defn ^:after-load re-render []
+  (mount))
+
+(defonce start-up (do (mount) true))
